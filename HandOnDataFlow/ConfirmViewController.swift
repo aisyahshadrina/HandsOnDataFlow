b7f76ee614da9de6f0b0886cf5618ec4147ad0f8
Aisyah Shadrina <aisyahshadrina@gmail.com>
2019-05-16 11:35:19 +0700

    View Controller & Confirm View Controller

---
+++ HandOnDataFlow/ConfirmViewController.swift
@@ -0,0 +1,36 @@
+//
+//  ConfirmViewController.swift
+//  HandOnDataFlow
+//
+//  Created by Aisyah Shadrina on 16/05/19.
+//  Copyright © 2019 Aisyah Shadrina. All rights reserved.
+//
+
+import UIKit
+
+class ConfirmViewController: UIViewController {
+
+    @IBOutlet weak var nameLabel: UILabel!
+    
+    var fullname: String!
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        
+        nameLabel.text = fullname
+
+        // Do any additional setup after loading the view.
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destination.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
